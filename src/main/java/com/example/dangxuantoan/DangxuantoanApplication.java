f2336fce3f8c992872e0099dee694326e6fa0854
DangXuanToan <renboy122333444@gmail.com>
2026-03-26 14:20:40 +0700

    first commit

---
+++ src/main/java/com/example/dangxuantoan/DangxuantoanApplication.java
@@ -0,0 +1,15 @@
+package com.example.dangxuantoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangxuantoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangxuantoanApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/example/dangxuantoan/DangxuantoanApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.dangxuantoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangxuantoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
